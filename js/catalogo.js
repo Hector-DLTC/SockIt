fb40260a69b9503d32935ddec4b376756c3132fe
Kazuo <kazuominakata@gmail.com>
2022-06-15 18:15:03 -0500

    Se agrego los archivos de catalogo.htm y js

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,15 +1,15 @@
 
 //JSON
-let products = [
-    {
-    "id": 1,
+let products = [{
+
+    "id": {
     "brand": "Happy Socks",
     "price": 109.95,
     "description": "Panther, 1 par, 85 % cachemir, altura hasta la rodilla",
     "category": "largos",
     "image": "https://m.media-amazon.com/images/I/61OUwrv4dpL._AC_UL1000_.jpg",
     "color": "white" 
-    },
+}}+
     {
         "id": 2,
         "brand": "Pantherella",
@@ -18,7 +18,7 @@ let products = [
         "category": "hastaRodilla",
         "image": "https://m.media-amazon.com/images/I/81BvqZs1BlL._AC_UL1500_.jpg",
         "color": "grey" 
-    },
+    }+
     {
         "id": 3,
         "brand": "Stance",
@@ -27,7 +27,7 @@ let products = [
         "category": "largos",
         "image": "https://m.media-amazon.com/images/I/81J0mLhOnQL._AC_UL1500_.jpg",
         "color": "red" 
-    },
+    }+
     {
         "id": 4,
         "brand": "Gallo",
@@ -36,7 +36,7 @@ let products = [
         "category": "largos",
         "image": "https://m.media-amazon.com/images/I/71iNKN5TZIL._AC_UL1500_.jpg",
         "color": "black" 
-    },
+    }+
     {
         "id": 5,
         "brand": "K-BELL",
@@ -45,7 +45,7 @@ let products = [
         "category": "largos",
         "image": "https://m.media-amazon.com/images/I/81JaehXDL+L._AC_UL1500_.jpg",
         "color": "blue" 
-    },
+    }+
     {
         "id": 5,
         "brand": "Carter",
@@ -54,7 +54,7 @@ let products = [
         "category": "cortos",
         "image": "https://m.media-amazon.com/images/I/812AhQT8-yL._AC_UL1500_.jpg",
         "color": "mix" 
-    },
+    }+
     {
         "id": 6,
         "brand": "PUMA",
@@ -63,7 +63,7 @@ let products = [
         "category": "cortos",
         "image": "https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg",
         "color": "black" 
-    },
+    }+
     {
         "id": 7,
         "brand": "PUMA",
@@ -72,7 +72,7 @@ let products = [
         "category": "cortos",
         "image": "https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg",
         "color": "black" 
-    },
+    }+
     {
         "id": 8,
         "brand": "Wilson",
@@ -81,7 +81,7 @@ let products = [
         "category": "cortos",
         "image": "https://m.media-amazon.com/images/I/51E5Uoqy16L._AC_SL1200_.jpg",
         "color": "black" 
-    },
+    }+
     {
         "id": 9,
         "brand": "FruitLoom",
@@ -90,7 +90,7 @@ let products = [
         "category": "cortos",
         "image": "https://m.media-amazon.com/images/I/917aNYmnaJL._AC_SL1500_.jpg",
         "color": "black" 
-    },
+    }+
     {
         "id": 10,
         "brand": "Timberland",
@@ -102,4 +102,97 @@ let products = [
     }
 
 ]
-   
+function addItem(products){
+    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+products.image +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+products.brand+'</h5>\n' +
+        '        <h6 class="card-title">'+products.category+'</h6>\n' +
+        '        <p class="card-text">'+products.description+'</p>\n' +
+        '        <p class="card-text">'+products.price+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+    const itemsContainer = document.getElementById("list-items");
+    itemsContainer.innerHTML += itemHTML;
+}
+
+
+
+addItem({'brand':'Happy Socks',
+    'image':'https://m.media-amazon.com/images/I/61OUwrv4dpL._AC_UL1000_.jpg',
+    'description':'Panther, 1 par, 85 % cachemir, altura hasta la rodilla',
+    'price':'1120',
+    'category':'Large'
+});
+addItem({'brand':'Pantherella',
+    'image':'https://m.media-amazon.com/images/I/81BvqZs1BlL._AC_UL1500_.jpg',
+    'description':'Happy Socks Party Animal Singing Birthday Gift Box Calcetines para Mujer',
+    'price':'1120',
+    'category':'Hasta la rodilla'
+});
+addItem({'brand':'Stance',
+    'image':'https://m.media-amazon.com/images/I/81J0mLhOnQL._AC_UL1500_.jpg',
+    'description':'Stance Hombres Calcetines The Fourth',
+    'price':'1120',
+    'category':'Hasta la rodilla'
+});
+addItem({'brand':'Gallo',
+    'image':'https://m.media-amazon.com/images/I/71iNKN5TZIL._AC_UL1500_.jpg',
+    'description':'Gallo Calcetines largos vestir hombre rayas colores',
+    'price':'1120',
+    'category':'Hasta la rodilla'
+});
+addItem({'brand':'K-BELL',
+    'image':'https://m.media-amazon.com/images/I/81JaehXDL+L._AC_UL1500_.jpg',
+    'description':'K. Bell azul calcetines de mujer',
+    'price':'1120',
+    'category':'largos'
+});
+addItem({'brand':'Carter',
+    'image':'https://m.media-amazon.com/images/I/812AhQT8-yL._AC_UL1500_.jpg',
+    'description':'Simple Joys by Carter Calcetines Deportivos Bebé-Niños, Pack de 12',
+    'price':'1120',
+    'category':'cortos'
+});
+addItem({'brand':'PUMA',
+    'image':'https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg',
+    'description':'PUMA Calcetines de corte bajo para hombre, 8 unidades',
+    'price':'1120',
+    'category':'cortos'
+});
+addItem({'brand':'PUMA',
+    'image':'https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg',
+    'description':'PUMA Calcetines de corte bajo para hombre, 8 unidades',
+    'price':'1120',
+    'category':'cortos'
+});
+addItem({'brand':'PUMA',
+    'image':'https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg',
+    'description':'PUMA Calcetines de corte bajo para hombre, 8 unidades',
+    'price':'1120',
+    'category':'cortos'
+});
+addItem({'brand':'PUMA',
+    'image':'https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg',
+    'description':'PUMA Calcetines de corte bajo para hombre, 8 unidades',
+    'price':'1120',
+    'category':'cortos'
+});
+addItem({'brand':'PUMA',
+    'image':'https://m.media-amazon.com/images/I/81M9mOv4YJS._AC_SL1500_.jpg',
+    'description':'PUMA Calcetines de corte bajo para hombre, 8 unidades',
+    'price':'1120',
+    'category':'cortos'
+});
+// for (let i = 0; i < JSON.parse(id); i++){
+// addItem(`${brand} + ${image} + ${description} + ${price} + ${category} `
+// )};
+
+// let data = JSON.stringify(products);
+
+// for (let key in data.id){
+//     console.log(key, data.id[0]);
+// }
+
