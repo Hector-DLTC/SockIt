77d34b61cbc2aefa848f0623285e6c4e781e75e6
Kazuo <kazuominakata@gmail.com>
2022-06-23 13:01:25 -0500

    Se modifico contactanos.js y productosJSON.js

--- js/productosJSON.js
+++ js/productosJSON.js
@@ -9,7 +9,7 @@ let Precio = document.getElementById("precio");
 let jsonStr =
   '{"theTeam":[{"teamId":"1","status":"pending"},{"teamId":"2","status":"member"},{"teamId":"3","status":"member"}]}';
   const obj = JSON.parse(jsonStr);
-  JSON.parse(localStorage.getItem("jsonStr"));
+//   JSON.parse(localStorage.getItem("jsonStr"));
 Agregar.addEventListener("click", (event) => {
     event.preventDefault();
     if([Producto.value,Descripcion.value,Precio.value].includes("")){
@@ -18,10 +18,10 @@ Agregar.addEventListener("click", (event) => {
     } else{
    
     obj["theTeam"].push({ product: `${Producto.value}`, description: `${Descripcion.value}`, price : `${Precio.value}`});
-    localStorage.setItem( "jsonStr", jsonStr = JSON.stringify(obj));
-    
-    
-}
+    localStorage.setItem( "jsonStr", jsonStr += JSON.stringify(obj));
+    JSON.parse(localStorage.getItem("jsonStr"));
+    // jsonStr += JSON.parse(localStorage.getItem("jsonStr"));
+    } location.reload();
 }
 )
 window.addEventListener("load", function(){
