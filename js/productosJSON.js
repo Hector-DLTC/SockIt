972173b605660d37ef38f67d5b9ca9f67ea32327
LuisMiguel <torresvaldezluismiguel@gmail.com>
2022-06-23 12:35:58 -0500

    Se agrego la funcion de localStorage con el JSON a catalogo.js y la funcion para llenar cartas en el catalogo con el JSON

--- js/productosJSON.js
+++ js/productosJSON.js
@@ -3,27 +3,26 @@ let Producto = document.getElementById("product");
 let Descripcion = document.getElementById("textdesc");
 let Precio = document.getElementById("precio");
 
+let jsonStr ='{"Items":[]}';
 
 
+let obj = JSON.parse(jsonStr);
 
-let jsonStr =
-  '{"theTeam":[{"teamId":"1","status":"pending"},{"teamId":"2","status":"member"},{"teamId":"3","status":"member"}]}';
-  const obj = JSON.parse(jsonStr);
-  JSON.parse(localStorage.getItem("jsonStr"));
 Agregar.addEventListener("click", (event) => {
     event.preventDefault();
     if([Producto.value,Descripcion.value,Precio.value].includes("")){
-        console.log("Hay al menos un campo vacio...")
+        console.log("Hay al menos un campo vacio...");
         //Aqui van las validaciones....
     } else{
-   
-    obj["theTeam"].push({ product: `${Producto.value}`, description: `${Descripcion.value}`, price : `${Precio.value}`});
+    obj["Items"].push({ product: `${Producto.value}`, description: `${Descripcion.value}`, price : `${Precio.value}`});
     localStorage.setItem( "jsonStr", jsonStr = JSON.stringify(obj));
-    
-    
-}
-}
-)
+    }
+    location.reload();
+    }
+);
 window.addEventListener("load", function(){
-    console.log(JSON.parse(localStorage.getItem("jsonStr")));
-})
+    if(["Items"]!=null){
+        console.log(JSON.parse(localStorage.getItem("jsonStr")));
+        obj=JSON.parse(localStorage.getItem("jsonStr"));
+    }
+});
