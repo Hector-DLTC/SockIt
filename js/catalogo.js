baea045256f0b8921962a409b49c09603adf6393
LuisMiguel <torresvaldezluismiguel@gmail.com>
2022-06-30 11:00:21 -0500

    Modificaciones a paginas con respecto al redondeado de los campos, contenedores y botones, ademas de que se agregaron iconos para añadir al catalogo y regresar en añadir producto

--- js/catalogo.js
+++ js/catalogo.js
@@ -5,8 +5,8 @@ console.log(products);
 function addItem(products){
     console.log(products);
     const itemHTML = 
-        '<div class="card" style="width: 20%; min-width:150px; margin:10px;">\n' +
-        '    <img src="'+products.img +'" class="card-img-top" alt="image" style="height: auto;max-width: 100%;vertical-align: middle;">\n' +
+        '<div class="card" style="width: 100%; min-width:150px; margin:0px;">\n' +
+        '    <img src="'+products.img +'" class="card-img-top" alt="image" style="height:200px;max-width: 100%;vertical-align: middle;">\n' +
         '    <div class="card-body">\n' +
         '        <h5 class="card-title">'+products.product+'</h5>\n' +
         // '        <h6 class="card-title">'+products.category+'</h6>\n' +
@@ -14,8 +14,7 @@ function addItem(products){
         '        <p class="card-text"style=" text-align: right;" > $'+products.price+'</p>\n' +
         '        <div><a href="#" class="btn btn-primary" style="position: absolute;top: 86%;">Añadir</a></div>\n' +
         '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
+        '</div>\n' ;
     const itemsContainer = document.getElementById("list-items");
     itemsContainer.innerHTML += itemHTML;
 }
@@ -45,3 +44,11 @@ function abrirModal(){
 function cerrarModal(){
     modal.style.display="none";
 }
+
+// let añadirProd =document.getElementById("añadirP");
+// let btnAgregarP=document.getElementById("buttform");
+
+// añadirProd.addEventListener('click',function añadirP(){
+//     btnAgregarP.click();
+// });
+
