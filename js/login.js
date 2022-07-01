b622d84f4ddfc801884a3dc4e2e872417ebfc7eb
LuisMiguel <torresvaldezluismiguel@gmail.com>
2022-07-01 18:17:25 -0500

    Se modifico el js de inicio de sesion

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ let emailLog = document.getElementById("emailLogin");
 let passwordLog = document.getElementById("passwordLogin");
 let registroInfo = JSON.parse(localStorage.getItem("regis"));
 let btnIncio = document.getElementById("btninicio");
+let valid;
 
 btnIncio.addEventListener('click', (event)=>{
     event.preventDefault();
@@ -11,11 +12,28 @@ btnIncio.addEventListener('click', (event)=>{
     console.log(registroInfo.Registro[0].password);
     console.log(emailLog.value);
     console.log(passwordLog.value);
-if (emailLog.value == registroInfo.Registro[0].email && passwordLog.value == registroInfo.Registro[0].password){
-    alert("INICIASTE CORRECTAMENTE");
-}else{
-    alert("LA INFO NO CONCUERDA");
-}
+
+    for (let data of registroInfo.Registro){
+        if (emailLog.value == data.email && passwordLog.value == data.password){
+            alert("INICIASTE CORRECTAMENTE");
+            valid=false;
+            console.log(valid);
+        }
+        // }if (emailLog.value != data.email && passwordLog.value != data.password){
+        //     alert("NO INICIASTE CORRECTAMENTE");
+        //     valid=false;
+        //     console.log(valid);
+        // }
+        
+    }
+    if (valid){
+        alert("LA INFO NO CONCUERDA");
+        console.log(valid);
+    }else{
+        return;
+    }
+    
+    console.log(valid);
 });
 
 
