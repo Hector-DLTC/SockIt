28b281161adcf932ac9b219bf84d6d6fb238f8b1
LuisMiguel <torresvaldezluismiguel@gmail.com>
2022-07-02 14:02:51 -0500

    Se modifico el ciclo for each de login.js ademas de los if de la misma pagina

--- js/login.js
+++ js/login.js
@@ -2,38 +2,30 @@ let emailLog = document.getElementById("emailLogin");
 let passwordLog = document.getElementById("passwordLogin");
 let registroInfo = JSON.parse(localStorage.getItem("regis"));
 let btnIncio = document.getElementById("btninicio");
-let valid;
+let valid=true;
 
 btnIncio.addEventListener('click', (event)=>{
     event.preventDefault();
-  
-    console.log(registroInfo);
-    console.log(registroInfo.Registro[0].email);
-    console.log(registroInfo.Registro[0].password);
-    console.log(emailLog.value);
-    console.log(passwordLog.value);
 
     for (let data of registroInfo.Registro){
         if (emailLog.value == data.email && passwordLog.value == data.password){
-            alert("INICIASTE CORRECTAMENTE");
+            valid=true;
+            console.log(valid);
+        }
+        else {
             valid=false;
             console.log(valid);
         }
-        // }if (emailLog.value != data.email && passwordLog.value != data.password){
-        //     alert("NO INICIASTE CORRECTAMENTE");
-        //     valid=false;
-        //     console.log(valid);
-        // }
         
     }
-    if (valid){
-        alert("LA INFO NO CONCUERDA");
-        console.log(valid);
+    if (valid==true){
+        alert("LA INFO CONCUERDA...");
+        
     }else{
-        return;
+        alert("LA INFO NO CONCUERDA...");
     }
     
-    console.log(valid);
+    
 });
 
 
